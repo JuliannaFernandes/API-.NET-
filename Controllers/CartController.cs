a277b8fca11c0c6b36912534e5fcca172787cd9b
Julianna Fernandes <juliannabf29@gmail.com>
2025-01-22 23:16:07 -0300

    feat: creating listing method

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -12,6 +12,45 @@ namespace Crud.Controllers;
 [Route("v1")]
 public class CartController : ControllerBase
 {
+  //metodo para listar todos os carrinhos
+  [HttpGet("carts")]
+  public async Task<IActionResult> GetCartsAsync(
+  [FromServices] AppDbContext context)
+  {
+    var carts = await context.Carts
+      .Include(c => c.Item)
+      .ThenInclude(i => i.Product)
+      .Select(cart => new
+      {
+        cart.Id,
+        cart.ItemId,
+        Item = new
+        {
+          cart.Item.Quantity,
+          cart.Item.UnitMeasure,
+          Product = new
+          {
+            cart.Item.Product.Name
+          }
+        }
+      })
+      .ToListAsync();
+    return Ok(carts);
+  }
+ 
+  //metodo para buscar um carrinho pelo id
+  [HttpGet("carts/{id}")]
+  public async Task<IActionResult> GetByIdAsync(
+    [FromServices] AppDbContext context,
+    [FromRoute] int id)
+  {
+    var cart = await context
+      .Carts
+      .AsNoTracking()
+      .FirstOrDefaultAsync(x => x.Id == id);
+    return cart == null ? NotFound(new{message = "Carrinho não encontrado"}) : Ok(cart);
+  }
+ 
   
 }
 
--- obj/Debug/net9.0/Crud.AssemblyInfo.cs
+++ obj/Debug/net9.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7d01e5552fcaa30caa086282eb7fef484666d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a76a9ea9343bbb3d4aedb8a13e2748ef0e91af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
